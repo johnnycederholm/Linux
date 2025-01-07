de668771df63fd8740d90c1235a68643c94bf21b
Johnny <johnny.k.cederholm@gmail.com>
2025-01-07 22:02:51 +0100

    Symlink dotfiles for KDE

---
+++ Configuration/Symlink-dotfiles.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ln -s -f ~/Linux/Dotfiles/KDE/plasma-org.kde.plasma.desktop-appletsrc ~/.config/plasma-org.kde.plasma.desktop-appletsrc
+ln -s -f ~/Linux/Dotfiles/KDE/dolphinrc ~/.config/dolphinrc
+ln -s -f ~/Linux/Dotfiles/KDE/kdeglobals ~/.config/kdeglobals
+ln -s -f ~/Linux/Dotfiles/KDE/kglobalshortcutsrc ~/.config/kglobalshortcutsrc
