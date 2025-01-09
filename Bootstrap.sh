f772e90321d546798b3a52da479d2fa9fdf9531d
Johnny <johnny.k.cederholm@gmail.com>
2025-01-09 14:08:40 +0100

    Replace usage of globstar with find

--- Bootstrap.sh
+++ Bootstrap.sh
@@ -2,19 +2,6 @@
 sudo apt update && sudo apt install -y git
 git clone https://github.com/johnnycederholm/Linux.git
 
-scripts=(
-    ./Linux/**/*.sh
-    ./Linux/**/*.ps1
-)
-
-# Enable expansion of subdirectories
-# by using the ** glob pattern
-shopt -s globstar
-
 echo "Make scripts executable..."
-for file in "${scripts[@]}";do
-    if [ -f $file ]; then
-        echo "$file"
-        chmod +x ${file}
-    fi
-done
+find ./ -maxdepth 5 -type f -name \*.sh -exec chmod --changes +x {} \;
+find ./ -maxdepth 5 -type f -name \*.ps1 -exec chmod --changes +x {} \;
