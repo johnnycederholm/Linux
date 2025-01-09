ea4bf4db9669259d6d125fffabbd64387e6fe106
Johnny <johnny.k.cederholm@gmail.com>
2025-01-09 13:00:37 +0100

    Add dotfiles for Konsole

--- Configuration/Symlink-dotfiles.sh
+++ Configuration/Symlink-dotfiles.sh
@@ -3,6 +3,7 @@ kdeDotfiles=(
     dolphinrc
     kdeglobals
     kglobalshortcutsrc
+    konsolerc
     kwinrc
     mimeapps.list
     plasma-org.kde.plasma.desktop-appletsrc
@@ -12,3 +13,5 @@ kdeDotfiles=(
 for dotfile in "${kdeDotfiles[@]}";do
     ln -s -f ~/Linux/Dotfiles/KDE/${dotfile} ~/.config/${dotfile}
 done
+
+ln -s -f ~/Linux/Dotfiles/KDE/konsole.Bash.profile ~/.local/share/konsole/Bash.profile
---
+++ Dotfiles/KDE/konsole.Bash.profile
@@ -0,0 +1,10 @@
+[Appearance]
+ColorScheme=Solarized
+Font=JetBrainsMono Nerd Font Mono,11,-1,5,50,0,0,0,0,0
+
+[Cursor Options]
+CursorShape=0
+
+[General]
+Name=Bash
+Parent=FALLBACK/
