b3b493ab28be1c0a7b0a2c40b1433962b1b26160
Johnny <johnny.k.cederholm@gmail.com>
2025-01-09 21:29:48 +0100

    Add script for install Dropbox

---
+++ Scripts/Applications/Common/Install-dropbox.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source ./Scripts/Applications/Versions.sh
+
+echo "Install Dropbox..."
+
+wget https://linux.dropboxstatic.com/packages/debian/dropbox_${DROPBOX_VERSION}_amd64.deb
+dpkg -i dropbox_${DROPBOX_VERSION}_amd64.deb
+rm -rf dropbox_${DROPBOX_VERSION}_amd64.deb
