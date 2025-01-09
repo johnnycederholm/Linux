90a0b090525acf7b3db99bba665937516f9af08a
Johnny <johnny.k.cederholm@gmail.com>
2025-01-09 22:21:49 +0100

    Add base colors

--- Common.sh
+++ Common.sh
@@ -1,3 +1,13 @@
+NC='\033[0m'
+Black='\033[0;30m'
+Red='\033[0;31m' 
+Green='\033[0;32m'
+Yellow='\033[0;33m'
+Blue='\033[0;34m'
+Purple='\033[0;35m'
+Cyan='\033[0;36m'
+White='\033[0;37m'
+
 makeScriptsExecutable() {
     echo "Make scripts executable..."
     find ./ -maxdepth 5 -type f -name \*.sh -exec chmod --changes +x {} \;
@@ -9,8 +19,5 @@ reboot () {
 }
 
 info () {
-    NC='\033[0m'
-    Green='\033[0;32m'
-
     echo -e "${Green}$1${NC}"
 }
