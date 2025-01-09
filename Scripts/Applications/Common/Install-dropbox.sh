e57be7e66ce61876212ea6c73cef26d3244e6f63
Johnny <johnny.k.cederholm@gmail.com>
2025-01-09 21:51:42 +0100

    Use apt install when installing Dropbox

--- Scripts/Applications/Common/Install-dropbox.sh
+++ Scripts/Applications/Common/Install-dropbox.sh
@@ -4,5 +4,5 @@ source ./Scripts/Applications/Versions.sh
 echo "Install Dropbox..."
 
 wget https://linux.dropboxstatic.com/packages/ubuntu/dropbox_${DROPBOX_VERSION}_amd64.deb
-dpkg -i dropbox_${DROPBOX_VERSION}_amd64.deb
+apt install -y ./dropbox_${DROPBOX_VERSION}_amd64.deb
 rm -rf dropbox_${DROPBOX_VERSION}_amd64.deb
